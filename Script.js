48c4487ea35753406a71a74964779eefeb289843
KarolinaMika <karolina.mikailaite@gmail.com>
2021-12-18 00:27:17 +0200

    burger-without-pop-up

---
+++ Script.js
@@ -0,0 +1,7 @@
+let menuTrigger = document.getElementById("mobile-menu-trigger");
+let mobileMenu = document.querySelector("header nav");
+
+menuTrigger.addEventListener('click', function() {
+  this.classList.toggle("active");
+  mobileMenu.classList.toggle("active");
+});
